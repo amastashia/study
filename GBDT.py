d85c78b331273792fa7ca81e47e1620514fb3f01
TAIYO <49736579+amastashia@users.noreply.github.com>
2023-10-21 22:19:12 +0900

    Add files via upload

---
+++ GBDT.py
@@ -0,0 +1,32 @@
+from xgboost import XGBClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score
+import pandas as pd
+
+#parameters
+data_name = 'biodeg_norm'
+
+#preprocessing
+dir = './dataset/'+data_name+'.csv'
+kf = KFold(n_splits=5, shuffle=True, random_state=0)
+df = pd.read_csv(dir, index_col=0)
+score_list = []
+
+#execution
+for train, test in kf.split(df):
+    df_train = df.iloc[train]
+    df_test = df.iloc[test]
+    train_X = df_train.iloc[:, :-1]
+    train_y = df_train.iloc[:, -1]
+    test_X = df_test.iloc[:, :-1]
+    test_y = df_test.iloc[:, -1]
+    model = XGBClassifier(n_estimators = 100)
+    model.fit(train_X, train_y)
+    predict_y = model.predict(test_X)
+    score = f1_score(test_y, predict_y)
+    score_list.append(score)
+print(score_list)
+
+#result
+#not good scores with Pima-Indians-diabetes, monk2 and seismic-bumps.
+#especially seismic-bumps is very bad.
---
+++ kernel-SVM.py
@@ -0,0 +1,28 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score
+import pandas as pd
+
+#parameters
+data_name = 'breast-cancer_norm'
+
+#preprocessing
+dir = './dataset/'+data_name+'.csv'
+kf = KFold(n_splits=5, shuffle=True, random_state=0)
+df = pd.read_csv(dir, index_col=0)
+score_list = []
+
+#execution
+for train, test in kf.split(df):
+    df_train = df.iloc[train]
+    df_test = df.iloc[test]
+    train_X = df_train.iloc[:, :-1]
+    train_y = df_train.iloc[:, -1]
+    test_X = df_test.iloc[:, :-1]
+    test_y = df_test.iloc[:, -1]
+    model = SVC(kernel='poly', degree=5, coef0=1)
+    model.fit(train_X, train_y)
+    predict_y = model.predict(test_X)
+    score = f1_score(test_y, predict_y)
+    score_list.append(score)
+print(score_list)
